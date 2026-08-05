a30ef203b988ccbba8cd8b4ead12fffe5372e1a4
Sadriwala Fakhruddin Abdullah <fakhruddinsadriwala85@gmail.com>
2026-08-05 21:40:36 +0530

    Added test suite for Task 5

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from data.app import app
+from data.app import app  # kyunki app.py data folder ke andar hai
 
 def test_header_present():
     header = app.layout.children[0]
@@ -7,7 +7,6 @@ def test_header_present():
 def test_visualisation_present():
     graph = app.layout.children[2]
     assert graph.id == "sales-chart"
-
 
 def test_region_picker_present():
     radio = app.layout.children[1]
