d8ed324d4348d7bd055d939a1e10fe8084cb88a4
Sadriwala Fakhruddin Abdullah <fakhruddinsadriwala85@gmail.com>
2026-08-05 16:09:04 +0530

    Updated Dash app as per Task 3 requirements

--- data/app.py
+++ data/app.py
@@ -1,8 +1,27 @@
+import pandas as pd
+import plotly.express as px
 import dash
-from dash import html
+from dash import dcc, html
+
+df = pd.read_csv("processed_sales.csv")
+df["date"] = pd.to_datetime(df["date"])
+df = df.sort_values("date")
+
+fig = px.line(
+    df,
+    x="date",
+    y="Sales",
+    title="Pink Morsel Sales Over Time",
+    labels={"date": "Date", "Sales": "Sales"},
+    template="plotly_white"
+)
 
 app = dash.Dash(__name__)
-app.layout = html.Div("Setup Successful!")
+
+app.layout = html.Div([
+    html.H1("Soul Foods Sales Visualiser"),
+    dcc.Graph(figure=fig)
+])
 
 if __name__ == "__main__":
     app.run(debug=True)
